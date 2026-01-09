e548a30b691b65b8c65a4b415a671791f4f2255f
Mher Saribekyan <saribekyanmher@gmail.com>
2026-01-09 14:19:52 +0400

    minor changes

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import pandas as pd
 import re
 import os
 
-datasets = ["text.csv", "1.csv"]
+datasets = os.listdir("datasets")
 
 def upload(filename, file):
     if not filename:
@@ -20,9 +21,20 @@ def upload(filename, file):
     if file is None:
         st.write("File not uploaded")
         return None
+    df = pd.read_csv(file)
 
-    with open(os.path.join("datasets", filename + ".csv", ),"wb") as f:
-        f.write(file.getbuffer())
+    expected_clumns = ['feature_1', 'feature_2', 'feature_3', 'feature_4', 'feature_5']
+    for col in expected_clumns:
+        if col not in df.columns:
+            st.write(f"There is no column: {col}")
+            return None
+
+    df = df.dropna(subset=expected_clumns)
+    df.to_csv(os.path.join("datasets", filename + ".csv"), encoding='utf-8', index=False)
+    datasets.append(filename + ".csv")
+
+    return filename + ".csv"
+
 
 st.title("Re-Code: Cluster")
 
@@ -43,9 +55,20 @@ elif dataset_method == "Select dataset from list":
 if st.button("Run", key="button_run"):
     if dataset_method == "Upload dataset":
         dataset = upload(filename, file)
-    if dataset is not None:    
-        st.write("Using the dataset: " + dataset)
+    if dataset is None:
+        st.write("Dataset selection error")
     else:
-        st.write("Error")
+        st.write("Using the dataset: " + dataset)
+        st.write("Scaler: " + scaler_method)
+        st.write("Clusters: " + str(n_clusters))
+        st.write("Random State: " + str(random_state))
+
+        df = pd.read_csv(os.path.join("datasets", filename + ".csv"))
+        config = {
+            "scaler": scaler_method,
+            "clusters": n_clusters,
+            "random_state": random_state
+        }
+        run_id = run_clustering(df, config)
 
 
